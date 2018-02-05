0c12cc6febddf0abebff84e7a172ccb049be45eb
Naga Anusha Kolli <kolli.anusha9@gmail.com>
2018-02-05 23:47:16 +0000

    automatically backed up by learn

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -1,4 +1,7 @@
 function IcebreakerResponse(email){
   this.email = email
 }
-
+IcebreakerResponse.BatchCreate = function(data){
+  return data.map(function(userData){
+    return new IcebreakerResponse(userData[0], userData[1])
+  })
